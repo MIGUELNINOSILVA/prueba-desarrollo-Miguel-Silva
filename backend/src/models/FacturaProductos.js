0f5a5e16cc41bc7dd9c2e1fb39ec7ab756d67914
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-11-09 17:41:55 -0500

    feat: :art: Refactor FacturaProductos schema to use single object references.

--- backend/src/models/FacturaProductos.js
+++ backend/src/models/FacturaProductos.js
@@ -1,18 +1,14 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
 const facturaProductosSchema = new Schema({
-  id_factura: [
-    {
-      ref: "Factura",
-      type: Schema.Types.ObjectId
-    }
-  ],
-  id_producto: [
-    {
-      ref: "Producto",
-      type: Schema.Types.ObjectId
-    }
-  ]
+  id_factura: {
+    ref: "Factura",
+    type: Schema.Types.ObjectId,
+  },
+  id_producto: {
+    ref: "Producto",
+    type: Schema.Types.ObjectId, 
+  },
 });
 
 export default model("Factura_productos", facturaProductosSchema);
