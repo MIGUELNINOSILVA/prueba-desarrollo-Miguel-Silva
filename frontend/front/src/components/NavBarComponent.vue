1c27ffe3724fee9283ed19374fcef0e0d92d1eca
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-11-09 22:48:52 -0500

    feat: :lipstick: Add navbar component with company name and developer role

--- frontend/front/src/components/NavBarComponent.vue
+++ frontend/front/src/components/NavBarComponent.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Hola</h1>
+  <div class="navbar-container">
+    <div><span>Prueba</span> CAPP CO</div>
+    <div>Desarrollador Full Stack</div>
+  </div>
 </template>
+
+<style scoped>
+
+.navbar-container{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1rem 0;
+}
+span {
+  font-weight: bold;
+}
+</style>
