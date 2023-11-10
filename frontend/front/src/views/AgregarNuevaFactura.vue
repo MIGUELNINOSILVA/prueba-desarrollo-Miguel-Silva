dd7e7c5a7160e2ab9d5c84753d0d33d94cbd0e34
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-11-10 16:01:24 -0500

    feat: :sparkles: Add product selection and fetch products from API.

--- frontend/front/src/views/AgregarNuevaFactura.vue
+++ frontend/front/src/views/AgregarNuevaFactura.vue
@@ -2,36 +2,38 @@
   <div class="container-formulario">
     <form>
       <h2>Agregar nueva factura</h2>
-      <div class="form-group">
+      <div class="form-group mt-3">
         <label for="searchInput">Buscar cliente:</label>
         <input type="text" class="form-control" id="searchInput" v-model="searchQuery" @input="filterClientes">
       </div>
-      <div class="form-group">
-        <label for="exampleSelect">Selecciona el cliente:</label>
-        <select class="form-select" id="exampleSelect" v-model="selectedCliente">
+      <div class="form-group mt-3">
+        <label for="exampleSelect mt-2">Selecciona el cliente:</label>
+        <select class="form-select mt-2" id="exampleSelect" v-model="selectedCliente" required>
           <option value="seleccionar" selected>Seleccionar</option>
           <option v-for="cliente in filteredClientes" :key="cliente._id" :value="cliente._id">
             {{ cliente.nombre }} - {{ cliente.id_cliente }}
           </option>
         </select>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+      <div class="form-group mt-3">
+        <label for="productSelect">Selecciona el producto:</label>
+        <select class="form-select mt-2" id="productSelect" v-model="selectedProducto" required>
+          <option value="seleccionar" selected>Seleccionar</option>
+          <option v-for="producto in productos" :key="producto._id" :value="producto._id">
+            {{ producto.nombre }} - ${{ producto.precio }} Stock: {{ producto.stock }}
+          </option>
+        </select>
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue';
+import { ref, watchEffect, onMounted } from 'vue';
 
 const clientes = ref([]);
+const productos = ref([]);
 const searchQuery = ref('');
 const selectedCliente = ref('');
 const filteredClientes = ref([]);
@@ -46,6 +48,17 @@ const getClientes = async () => {
   }
 };
 
+const getProductos = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/producto');
+    const data = await response.json();
+    console.log(data.data);
+    productos.value = data.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 const filterClientes = () => {
   const query = searchQuery.value.toLowerCase();
   filteredClientes.value = clientes.value.filter(cliente =>
@@ -53,12 +66,14 @@ const filterClientes = () => {
   );
 };
 
-
 watchEffect(() => {
   filterClientes();
 });
 
-getClientes();
+onMounted(() => {
+  getClientes();
+  getProductos();
+});
 </script>
 
 <style scoped>
